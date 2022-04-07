abf3922869742290c5c6f3d503e12838ddc2900d
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-04-07 00:40:56 -0500

    added the api for openweather and it changes the untis to the imperial system

--- Assets/java.js
+++ Assets/java.js
@@ -18,8 +18,30 @@ var formSubmitHandler = function(event){
     }
 };
 
-var getCityInfo = function() {
-    console.log('hello world')
+var getCityInfo = function(city) {
+    //api pulls from the weather api and changes the units to the imperial system.
+    var apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=68452c2fbfaea6192b362a49b9ef2f1f"
+
+    fetch(apiUrl)
+        .then(function(response){
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayCity(data, city);
+                });
+            } 
+            else {
+                alert('Error: City not found');
+            }
+        })
+        .catch(function(error){
+            alert("Cannot connect to weather api");
+        });
+};
+
+var displayCity = function(city, searchTerm) {
+    console.log('This is temperature')
 }
 
 userFormEl.addEventListener('submit', formSubmitHandler);
