ac66204ca3fef3f37edb248f1c8b92170ce9d675
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-04-12 04:15:40 -0500

    5 day forecast populates with the correct parameters

--- Assets/java.js
+++ Assets/java.js
@@ -56,37 +56,26 @@ var displayCity = function(repos, searchTerm) {
     
     repoSearch.textContent = searchTerm //placeholder for date;
 
-    for (var i = 0; i <repos.lenght; i++) {
-
-        //gets the temperature from the api and replaces the text
-        var TempPlaceholder = document.querySelector("#temp-placeholder");
-        var reposTemp = repos[i].main.weather;
-        TempPlaceholder.innerHTML = reposTemp;
-
-        //gets the wind from the api and replaces the text
-        //gets the humidity from the api and replaces the text
-        //gets the UV-index from the api and replaces the text
-    }
     //gets the temperature from the api and replaces the text
-    var TempPlaceholder = document.querySelector("#temp-placeholder");
+    var tempPlaceholder = document.querySelector("#temp-placeholder");
     var reposTemp = repos.current.temp;
-    TempPlaceholder.innerHTML = "Temperature:  " + reposTemp + " ℉";
+    tempPlaceholder.innerHTML = "Temperature:  " + reposTemp + " ℉";
 
     //gets the humidity from the api and replaces the text
-    var TempPlaceholder = document.querySelector("#wind-placeholder");
+    var windPlaceholder = document.querySelector("#wind-placeholder");
     var reposWind = repos.current.humidity;
-    TempPlaceholder.innerHTML = "Wind:  :  " + reposWind + " mph";
+    windPlaceholder.innerHTML = "Wind:  :  " + reposWind + " mph";
 
     //gets the UV-index from the api and replaces the text
-    var TempPlaceholder = document.querySelector("#uv-placeholder");
+    var uvPlaceholder = document.querySelector("#uv-placeholder");
     var reposUV = repos.current.uvi;
-    TempPlaceholder.innerHTML = "UV-Index  :  " + reposUV;
+    uvPlaceholder.innerHTML = "UV-Index  :  " + reposUV;
 
     getFiveDay = function(fiveDay) {
         longitude = repos.lon;
         latitude =  repos.lat;
 
-        var fiveDayAPI ='https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=68452c2fbfaea6192b362a49b9ef2f1f'
+        var fiveDayAPI ='https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=68452c2fbfaea6192b362a49b9ef2f1f' + '&units=imperial'
 
         fetch(fiveDayAPI)
             .then(function(response){
@@ -94,6 +83,60 @@ var displayCity = function(repos, searchTerm) {
                     response.json().then(function(data){
                         console.log(data);
                         
+                        //gets data from api to create the first forecast
+                        var tempOne = document.querySelector("#temp-one");
+                        var windOne = document.querySelector("#wind-one")
+                        var humidityOne = document.querySelector("#humidity-one")
+                        var tempDataOne = data.list[1].main.temp;
+                        var windDataOne = data.list[1].wind.speed
+                        var humidityDataOne = data.list[1].main.humidity
+                        tempOne.innerHTML = 'Temperature: ' + tempDataOne + '℉';
+                        windOne.innerHTML = 'Wind:  ' + windDataOne + 'mph';
+                        humidityOne.innerHTML = 'Humidity: ' + humidityDataOne;
+
+                        //gets data from api to create the second forecast
+                        var tempTwo = document.querySelector("#temp-two");
+                        var windTwo = document.querySelector("#wind-two")
+                        var humidityTwo = document.querySelector("#humidity-two")
+                        var tempDataTwo = data.list[2].main.temp;
+                        var windDataTwo = data.list[2].wind.speed
+                        var humidityDataTwo = data.list[2].main.humidity
+                        tempTwo.innerHTML = 'Temperature: ' + tempDataTwo + '℉';
+                        windTwo.innerHTML = 'Wind:  ' + windDataTwo + 'mph';
+                        humidityTwo.innerHTML = 'Humidity: ' + humidityDataTwo;
+
+                        //gets data from api to create the third forecast
+                        var tempThree = document.querySelector("#temp-three");
+                        var windThree = document.querySelector("#wind-three")
+                        var humidityThree = document.querySelector("#humidity-three")
+                        var tempDataThree = data.list[3].main.temp;
+                        var windDataThree = data.list[3].wind.speed
+                        var humidityDataThree = data.list[3].main.humidity
+                        tempThree.innerHTML = 'Temperature: ' + tempDataThree + '℉';
+                        windThree.innerHTML = 'Wind:  ' + windDataThree + 'mph';
+                        humidityThree.innerHTML = 'Humidity: ' + humidityDataThree;
+
+                        //gets data from api to create the fourth forecast
+                        var tempFour = document.querySelector("#temp-four");
+                        var windFour = document.querySelector("#wind-four")
+                        var humidityFour = document.querySelector("#humidity-four")
+                        var tempDataFour = data.list[4].main.temp;
+                        var windDataFour = data.list[4].wind.speed
+                        var humidityDataFour = data.list[4].main.humidity
+                        tempFour.innerHTML = 'Temperature: ' + tempDataFour + '℉';
+                        windFour.innerHTML = 'Wind:  ' + windDataFour + 'mph';
+                        humidityFour.innerHTML = 'Humidity: ' + humidityDataFour;
+
+                        //gets data from api to create the fifth forecast
+                        var tempFive = document.querySelector("#temp-five");
+                        var windFive = document.querySelector("#wind-five")
+                        var humidityFive = document.querySelector("#humidity-five")
+                        var tempDataFive = data.list[5].main.temp;
+                        var windDataFive = data.list[5].wind.speed
+                        var humidityDataFive = data.list[5].main.humidity
+                        tempFive.innerHTML = 'Temperature: ' + tempDataFive + '℉';
+                        windFive.innerHTML = 'Wind:  ' + windDataFive + 'mph';
+                        humidityFive.innerHTML = 'Humidity: ' + humidityDataFive;
                     });
                 }
             })
